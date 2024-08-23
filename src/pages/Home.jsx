1a4a5ae38384d3fa135268d297bb1ab13172af7f
deepaksenhacker <care.deepaksen@gmail.com>
2024-08-23 22:34:53 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Layout from '../Components/Layout';
-import StoryBar from '../Components/StoryBar';
+
 import Post from '../Components/Post';
 import AllPosts from '../Components/AllPosts';
 import Marquee from '../Components/marquee';
-
+import StoryBar from '../Components/StoryBar';
 
 const Home = () => {
     return (
          <Layout>
             <>
             
-        <StoryBar/> 
             <Marquee/> 
+            <StoryBar/> 
+       
         <AllPosts/>  
            </>
         </Layout>
