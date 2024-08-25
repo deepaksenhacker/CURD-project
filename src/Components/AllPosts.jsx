ce60fb699c85cfde90d2db7ef7ac6089298b899a
deepaksenhacker <care.deepaksen@gmail.com>
2024-08-25 14:55:59 +0530

    h

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const AllPosts = () => {
 
    const context = useContext(MyContext);
-   const {getAllPosts ,AllPosts ,loader} = context;
+   const {getAllPosts ,AllPosts ,loader } = context;
    
    useEffect(()=>{
     getAllPosts()
@@ -13,7 +13,7 @@ const AllPosts = () => {
 
 
     return (
-        <div className='container h-fit  flex flex-wrap gap-6 justify-center '>
+        <div className='h-fit  flex flex-wrap gap-6 justify-center '>
         {/* single */}
               {loader?
               <>
@@ -32,33 +32,26 @@ const AllPosts = () => {
               {AllPosts.length>0?
              AllPosts.map((item,index)=>{
 
-              const {title,description,_id,image} =item;
+              const {title,description,_id,image ,user} =item;
               const keys = item.keywords
               return (
                 <div className="gap-2">
-                 <div key={index} className="post border-2 p-2 border-sky-400 w-full lg:w-60 md:w-52 sm:w-40 bg-blue-200 rounded-2xl ">
-                  <h5 className=" text-sky-900 text-center  text-sm font-bold font-serif">{title}</h5>
-                  <div className="tags flex gap-4 justify-center flex-wrap">
-                  {keys.map((item,index)=>{
-                          return(<>
-                          
-                         <h5 key={index} className=" w-fit  text-green-100 text-sm font-bold font-sans p-2 rounded-2  bg-dark">{item}</h5>
+                 <div key={index} className=" w-full  ">
+           <div className="flex mb-2 gap-3 ">
+                  <div className="w-7 h-7 rounded-full border-2 border-pink-700 overflow-hidden">
+                     <img src={user.profileImage} alt="" className='w-full h-full object-cover' />
 
-                          </>)
-                         })}
-
-                  </div>  
-           <div className="w-56  h-56  overflow-hidden" >
-             <img src={image}  alt="" className="border-1 shadow-md rounded-l-3xl p-2 w-full h-full object-cover  hover:scale-110 transition" />
-
+                  </div>
+                    <h5 className="font-bold ">@</h5>
+              
+            </div>       
+           <div className="  overflow-hidden" >
+           <Link to={`/posts/view/${_id}`} className="btn btn-dark w-100 " >
+             <img src={image}  alt="" className=" w-full h-full object-cover  hover:scale-110 transition" />
+             </Link>
             </div>     
-                  <p className="text-wrap text-white">{description}</p> 
-                 <div className="flex p-2">
-                 <Link to={`/posts/view/${_id}`} className="btn btn-dark w-100 " > <i className="fab fa-github " />  View</Link>
-                 <button className="btn btn-outline  text-white w-40"> <i className="fab fa-whatsapp" />  Share</button>
+                  <p className="text-wrap mt-1 p-1 font-bold">{description}  <i className="fab fa-share" /></p> 
                  
-                 </div>
-
 
 
               </div>
